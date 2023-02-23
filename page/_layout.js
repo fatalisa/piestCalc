e53027c20c2e164736ddf14a647a65e34c4eb2cb
Dan.Su <40448732+fatalisa@users.noreply.github.com>
2023-02-23 15:43:25 +0800

    wip

---
+++ page/_layout.js
@@ -0,0 +1,20 @@
+define([], function () {
+    
+    return {
+        component: 'Layout',
+        fit:true,
+    
+        sider:{
+            children:'222'
+        },
+
+        body: {
+          children: {
+            // component: 'Router',
+            // defaultPath: 'start',
+            children:'111'
+
+          },
+        },
+      }
+})
