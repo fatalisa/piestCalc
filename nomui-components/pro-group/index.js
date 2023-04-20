359b659f47f7895ea9771bffd1d1ede46b2ac605
Dan.Su <daniel.su@ctsmed.com>
2023-04-20 11:20:36 +0800

    wip

--- nomui-components/pro-group/index.js
+++ nomui-components/pro-group/index.js
@@ -83,31 +83,8 @@ define(['dragula',"css!/libs/dragula/dragula.min.css","css!/nomui-components/pro
         containers.push(n.querySelector('ul'))
       })
 
-  
-
       Dragula({
         containers:containers,
-        // isContainer: function (el) {
-        //   return false; // only elements in drake.containers will be taken into account
-        // },
-        // moves: function (el, source, handle, sibling) {
-        //   return true; // elements are always draggable by default
-        // },
-        // accepts: function (el, target, source, sibling) {
-        //   return true; // elements can be dropped in any of the `containers` by default
-        // },
-        // invalid: function (el, handle) {
-        //   return false; // don't prevent any drags from initiating by default
-        // },
-        // direction: 'vertical',             // Y axis is considered when determining where an element would be dropped
-        // copy: false,                       // elements are moved by default, not copied
-        // copySortSource: false,             // elements in copy-source containers can be reordered
-        // revertOnSpill: false,              // spilling will put the element back where it was dragged from, if this is true
-        // removeOnSpill: false,              // spilling will `.remove` the element, if this is true
-        // mirrorContainer: document.body,    // set the element that gets mirror elements appended
-        // ignoreInputTextSelection: true,     // allows users to select input text, see details below
-        // slideFactorX: 0,               // allows users to select the amount of movement on the X axis before it is considered a drag instead of a click
-        // slideFactorY: 0,               // allows users to select the amount of movement on the Y axis before it is considered a drag instead of a click
       })
     }
 
@@ -169,7 +146,26 @@ define(['dragula',"css!/libs/dragula/dragula.min.css","css!/nomui-components/pro
             },
             rows:[
               {
-                children:n.name
+                children:{
+                  component:'Flex',
+                  gutter:'small',
+                  attrs:{
+                    style:{
+                      padding:'0 .25rem .5rem .25rem'
+                    }
+                  },
+                  cols:[{
+                    children:n.name
+                  },
+                  {
+                    classes:{
+                      'pro-group-title-count':true
+                    },
+                    children:n.events && n.events.length || 0,
+                  }
+                
+                ]
+                }
               },
               {
                 component:'List',
@@ -224,10 +220,7 @@ define(['dragula',"css!/libs/dragula/dragula.min.css","css!/nomui-components/pro
 
     }
 
-    _getGroupItems(arr) {
-      if (!arr.length) return []
-      const data = arr.map()
-    }
+
   }
 
   return ProGroup;
