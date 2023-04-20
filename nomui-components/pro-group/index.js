5a2369e7f48eaeb15421eb4b9dc917dd812cd229
Dan.Su <daniel.su@ctsmed.com>
2023-04-20 11:02:21 +0800

    wip

--- nomui-components/pro-group/index.js
+++ nomui-components/pro-group/index.js
@@ -1,6 +1,13 @@
 define(['dragula',"css!/libs/dragula/dragula.min.css","css!/nomui-components/pro-group/index.css"], function (Dragula) {
 
-window.Dragula = Dragula
+
+
+  nomui.Icon.add(
+    'tasks',
+    `<svg t="1681958966318" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3003" width="1em" height="1em" fill="currentColor"><path d="M224 800.256L223.712 224H320v31.68c0 35.456 28.64 64.32 63.872 64.32h256.256A64.16 64.16 0 0 0 704 255.68V224l96-0.256L800.256 800 224 800.256zM640 192.32L640.128 256 384 255.68V192.32L383.872 192 640 192.32zM799.84 160H695.04c-11.072-19.04-31.424-32-54.912-32h-256.256c-23.488 0-43.808 12.928-54.912 32H223.712A63.776 63.776 0 0 0 160 223.744v576.512C160 835.392 188.608 864 223.744 864h576.512A63.84 63.84 0 0 0 864 800.256V223.744A64 64 0 0 0 799.84 160z" p-id="3004"></path><path d="M619.072 429.088l-151.744 165.888-62.112-69.6a32 32 0 1 0-47.744 42.624l85.696 96a32 32 0 0 0 23.68 10.688h0.192c8.96 0 17.536-3.776 23.616-10.4l175.648-192a32 32 0 0 0-47.232-43.2" p-id="3005"></path></svg>`,
+   'Uncategorized'
+  )
+
   
   class ProGroup extends nomui.Component {
     constructor(props, ...mixins) {
@@ -57,7 +64,9 @@ window.Dragula = Dragula
             onCreated:({inst})=>{
               this.mainList = inst
             },
-            // sortable: true,
+            classes:{
+              'pro-group-main':true
+            },
             items:groups
         }
       })
@@ -69,7 +78,7 @@ window.Dragula = Dragula
 
     _initDragable() {
       const containers = []
-      const list = this.element.querySelectorAll('.pro-drag-container')
+      const list = this.element.querySelectorAll('.pro-group-box-list')
       list.forEach(n=>{
         containers.push(n.querySelector('ul'))
       })
@@ -105,35 +114,46 @@ window.Dragula = Dragula
     _getGroupData() {
         const {data,itemRender} = this.props
 
-        const defaultRender = itemRender || function (args)  {
+        const defaultRender = itemRender || function ({itemData})  {
           
-          const {itemData} = args
           
+
           return {
             children: {
-              component:'Flex',
+              component:'List',
               align:'start',
+              gutter:'sm',
 
-              cols:[
+              items:[
                 {
-                  classes:{
-                    'pro-group-checkbox':true,
-                  
-                  },
-                  children:{
+                  component:'Flex',
+                  align:'center',
+                  cols:[{
                     component:'Icon',
-                    type:'check'
-                  }
-               
+                    type:'tasks',
+                    classes:{
+                      'pro-group-event-icon':true
+                    }
+                  },
+                { children:itemData.tasks}]
+                 
                 },
                 {
-                  rows:[
-                    {
-                      children:itemData.name
+                  component:'Flex',
+                  align:'center',
+                  cols:[{
+                    component:'Icon',
+                    type:'clock',
+                    classes:{
+                      'pro-group-event-icon':true
                     }
-                  ]
-                }
-
+                  },
+                { children:itemData.date}]
+                 
+                },
+  
+            
+             
               ]
             }
           }
@@ -144,21 +164,57 @@ window.Dragula = Dragula
         const list = data.map(n=>{
           return {
             component:'Flex',
+            classes:{
+              'pro-group-box':true
+            },
             rows:[
               {
                 children:n.name
               },
               {
                 component:'List',
-                // sortable:true,
                 classes:{
-                  'pro-drag-container':true,
-                  'container':true
+                  'pro-group-box-list':true,
                 },
                 cols:1,
-                itemRender:defaultRender,
+                itemRender:({itemData})=>{
+                  return {
+                    component:'Flex',
+                    classes:{
+                      'pro-group-card':true
+                    },
+                    gap:'small',
+                    rows:[
+                      {
+          
+                        align:'start',
+          
+                        cols:[
+                          {
+                            classes:{
+                              'pro-group-checkbox':true,
+                            
+                            },
+                            children:{
+                              component:'Icon',
+                              type:'check'
+                            }
+                          },
+                          {
+                            rows:[
+                              {
+                                children:itemData.name
+                              }
+                            ]
+                          }
+          
+                        ]
+                      },
+                      defaultRender({itemData})
+                    ]
 
-             
+                  }
+                },
                 data:n.events
               }
             ]
