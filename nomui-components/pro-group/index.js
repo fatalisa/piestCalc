52931d4df7dd42a7d1ca1cee7207f144ead25e15
Dan.Su <daniel.su@ctsmed.com>
2023-04-23 17:16:42 +0800

    wip

--- nomui-components/pro-group/index.js
+++ nomui-components/pro-group/index.js
@@ -3,7 +3,6 @@ define([
   'css!/libs/dragula/dragula.min.css',
   'css!/nomui-components/pro-group/index.css',
 ], function (Dragula) {
-
   nomui.Icon.add(
     'tasks',
     `<svg t="1681958966318" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3003" width="1em" height="1em" fill="currentColor"><path d="M224 800.256L223.712 224H320v31.68c0 35.456 28.64 64.32 63.872 64.32h256.256A64.16 64.16 0 0 0 704 255.68V224l96-0.256L800.256 800 224 800.256zM640 192.32L640.128 256 384 255.68V192.32L383.872 192 640 192.32zM799.84 160H695.04c-11.072-19.04-31.424-32-54.912-32h-256.256c-23.488 0-43.808 12.928-54.912 32H223.712A63.776 63.776 0 0 0 160 223.744v576.512C160 835.392 188.608 864 223.744 864h576.512A63.84 63.84 0 0 0 864 800.256V223.744A64 64 0 0 0 799.84 160z" p-id="3004"></path><path d="M619.072 429.088l-151.744 165.888-62.112-69.6a32 32 0 1 0-47.744 42.624l85.696 96a32 32 0 0 0 23.68 10.688h0.192c8.96 0 17.536-3.776 23.616-10.4l175.648-192a32 32 0 0 0-47.232-43.2" p-id="3005"></path></svg>`,
@@ -38,7 +37,7 @@ define([
                 tasks: 1,
                 eventRender: null,
               },
-            
+
               {
                 id: '00103',
                 name: '测试1',
@@ -99,6 +98,7 @@ define([
     }
 
     _config() {
+      const me = this
       const { data } = this.props
 
       this.setProps({
@@ -145,72 +145,89 @@ define([
                 },
                 this._renderEventList(itemData),
                 {
-                  children:{
-                    classes:{
-                      'pro-group-event-add':true
+                  children: {
+                    classes: {
+                      'pro-group-event-add': true,
                     },
-                  
-                    children:[
+
+                    children: [
                       {
-                        onCreated:({inst})=>{
+                        onCreated: ({ inst }) => {
                           inst.parent.btn = inst
                         },
-                        onClick:({sender})=>{
+                        onClick: ({ sender }) => {
                           sender.hide()
                           sender.parent.inputPanel.show()
+                          sender.parent.parent.parent.element.classList.add(
+                            'pro-group-inputing'
+                          )
                         },
-                        classes:{
-                          'pro-group-event-add-btn':true
+                        classes: {
+                          'pro-group-event-add-btn': true,
                         },
-                        children:{
-                          component:'Icon',
-                           type:'plus'
-                        }
+                        children: {
+                          component: 'Icon',
+                          type: 'plus',
+                        },
                       },
                       {
-                       
-                        component:'Flex',
-                        onCreated:({inst})=>{
+                        component: 'Flex',
+                        onCreated: ({ inst }) => {
                           inst.parent.inputPanel = inst
                         },
-                        hidden:true,
-                        rows:[
+                        hidden: true,
+                        rows: [
                           {
-                            component:'MultilineTextbox',
-                            onCreated:({inst})=>{
+                            component: 'MultilineTextbox',
+                            onCreated: ({ inst }) => {
                               inst.parent.textArea = inst
                             },
-
                           },
                           {
-                            cols:[
+                            gutter:'small',
+                            justify:'end',
+                            cols: [
                               {
-                                component:'Button',
-                                text:'确定',
-                                type:'primary',
-                                onClick:({sender})=>{
-                                  
-                                  sender.parent.parent.parent.parent.parent.btn.show()
-                                  sender.parent.parent.parent.parent.parent.inputPanel.hide()
+                                component: 'Button',
+                                text: '确定',
+                                size:'small',
+                                type: 'primary',
+                                onClick: ({ sender }) => {
+                                  const n = sender.element.closest(
+                                    '.pro-group-event-add'
+                                  ).component
+
+                                  n.btn.show()
+                                  n.inputPanel.hide()
+
+                                  sender.element
+                                    .closest('.pro-group-inputing')
+                                    .classList.remove('pro-group-inputing')
                                 },
                               },
                               {
-                                component:'Button',
-                                text:'取消',
-                                onClick:({sender})=>{
-                                  sender.parent.parent.parent.parent.parent.btn.show()
-                                  sender.parent.parent.parent.parent.parent.inputPanel.hide()
-                         
-                       
+                                component: 'Button',
+                                text: '取消',
+                                size:'small',
+                                onClick: ({ sender }) => {
+                                  const n = sender.element.closest(
+                                    '.pro-group-event-add'
+                                  ).component
+
+                                  n.btn.show()
+                                  n.inputPanel.hide()
+                                  sender.element
+                                    .closest('.pro-group-inputing')
+                                    .classList.remove('pro-group-inputing')
                                 },
-                              }
-                            ]
-                          }
-                        ]
-                      }
-                    ]
-                  }
-                }
+                              },
+                            ],
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                },
               ],
             }
           },
@@ -230,7 +247,6 @@ define([
               component: 'List',
               align: 'start',
               gutter: 'sm',
-
 
               items: [
                 {
@@ -271,7 +287,7 @@ define([
         classes: {
           'pro-group-box-list': true,
         },
-    
+
         onCreated: ({ inst }) => {
           inst.parent.parent.parent.eventList = inst
         },
@@ -367,45 +383,36 @@ define([
         me._handleEventDrop()
       })
 
-      this.listDrag.on('cancel', function (el, container, source	) {
+      this.listDrag.on('cancel', function (el, container, source) {
         me._handleEventDrop()
       })
-
 
       this.listDrag.on('drag', function (el, source) {
         me._handleEventDrag()
       })
-
-      
     }
 
     _handleEventDrop() {
-      this.element.querySelectorAll('.pro-group-event-add').forEach(n=>{
+      this.element.querySelectorAll('.pro-group-event-add').forEach((n) => {
         n.classList.remove('hide')
       })
 
-      this.element.querySelectorAll('.pro-group-box-list').forEach(n=>{
+      this.element.querySelectorAll('.pro-group-box-list').forEach((n) => {
         n.closest('.nom-flex-item').classList.remove('strech')
       })
-      
+
       console.log(this.getData())
     }
 
-  
-
-
-
     _handleEventDrag() {
-      this.element.querySelectorAll('.pro-group-event-add').forEach(n=>{
+      this.element.querySelectorAll('.pro-group-event-add').forEach((n) => {
         n.classList.add('hide')
       })
 
-      this.element.querySelectorAll('.pro-group-box-list').forEach(n=>{
+      this.element.querySelectorAll('.pro-group-box-list').forEach((n) => {
         n.closest('.nom-flex-item').classList.add('strech')
       })
-      
     }
-
 
     getData() {
       const data = this.mainList.getAllItems().map((n) => {
